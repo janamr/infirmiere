ab02b6dea523e8007be9c0e5c009684fd280ccc1
Jana <jana@janaread.com>
2019-02-16 20:19:40 +0300

    database added

--- wp-content/themes/genesis-infirmiere/functions.php
+++ wp-content/themes/genesis-infirmiere/functions.php
@@ -10,5 +10,12 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
 }
+
+
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+// load child theme
+load_child_theme_textdomain('genesis-infirmiere');
+add_action( 'genesis_setup', 'genesis-infirmiere_setup' );
+
 ?>
