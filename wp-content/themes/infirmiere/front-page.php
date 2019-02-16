d5f5add6a113c2ca551ac8910f27fffb49ced5ae
Jana <jana@janaread.com>
2019-02-17 00:28:47 +0300

    made a mess

---
+++ wp-content/themes/infirmiere/front-page.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Front page template
+ *
+ * @package      infirmiere
+ * @author       janamr
+ * @link         http://www.janaread.com/
+ * @copyright    Copyright (c) 2019, janamr
+ * @license      GPL-3.0+
+ */
+
+genesis();
+
--- wp-content/themes/infirmiere/functions.php
+++ wp-content/themes/infirmiere/functions.php
@@ -1,167 +1,73 @@
 <?php
-/**
- * infirmiere functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package infirmiere
- */
-
-if ( ! function_exists( 'infirmiere_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function infirmiere_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on infirmiere, use a find and replace
-		 * to change 'infirmiere' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'infirmiere', get_template_directory() . '/languages' );
-
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
-
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
-
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
-
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'infirmiere' ),
-		) );
-
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
-
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'infirmiere_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
-
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
-
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
-	}
-endif;
-add_action( 'after_setup_theme', 'infirmiere_setup' );
 
 /**
- * Set the content width in pixels, based on the theme's design and stylesheet.
+ * Theme customizations
  *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
+ * @package      infirmiere
+ * @author       janamr
+ * @link         http://www.janaread.com/
+ * @copyright    Copyright (c) 2019, janamr
+ * @license      GPL-3.0+
  */
-function infirmiere_content_width() {
-	// This variable is intended to be overruled from themes.
-	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'infirmiere_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'infirmiere_content_width', 0 );
 
+// function my_theme_enqueue_styles() {
+
+//     $parent_style = 'genesis-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+
+//     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+//     wp_enqueue_style( 'child-style',
+//         get_stylesheet_directory_uri() . '/style.css',
+//         array( $parent_style ),
+//         wp_get_theme()->get('Version')
+//     );
+// }
+
+// Load child theme textdomain.
+load_child_theme_textdomain( 'infirmiere' );
+
+add_action( 'genesis_setup', 'infirmiere_setup' );
 /**
- * Register widget area.
+ * Theme setup.
  *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ * Attach all of the site-wide functions to the correct hooks and filters. All
+ * the functions themselves are defined below this setup function.
+ *
+ * @since 1.0.0
  */
-function infirmiere_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'infirmiere' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'infirmiere' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+function infirmiere_setup() {
+
+	// Define theme constants.
+	define( 'CHILD_THEME_NAME', 'infirmiere' );
+	define( 'CHILD_THEME_URL', 'http://github.com/janamr/infirmiere' );
+	define( 'CHILD_THEME_VERSION', '1.0.0' );
+
+	// Add HTML5 markup structure.
+	add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption'  ) );
+
+	// Add viewport meta tag for mobile browsers.
+	add_theme_support( 'genesis-responsive-viewport' );
+
+	// Add theme support for accessibility.
+	add_theme_support( 'genesis-accessibility', array(
+		'404-page',
+		'drop-down-menu',
+		'headings',
+		'rems',
+		'search-form',
+		'skip-links',
 	) );
-}
-add_action( 'widgets_init', 'infirmiere_widgets_init' );
 
-/**
- * Enqueue scripts and styles.
- */
-function infirmiere_scripts() {
-	wp_enqueue_style( 'infirmiere-style', get_stylesheet_uri() );
+	// Add theme support for footer widgets.
+	add_theme_support( 'genesis-footer-widgets', 3 );
 
-	wp_enqueue_script( 'infirmiere-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'infirmiere-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'infirmiere_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
 }
 
-/**
- * Load WooCommerce compatibility file.
- */
-if ( class_exists( 'WooCommerce' ) ) {
-	require get_template_directory() . '/inc/woocommerce.php';
-}
+
+// add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+// // load child theme
+// load_child_theme_textdomain('genesis-infirmiere');
+// add_action( 'genesis_setup', 'genesis-infirmiere_setup' );
+
+?>
